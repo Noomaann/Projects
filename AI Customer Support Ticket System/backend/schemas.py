9f8bacc886c66e835c6cfa5d7db28061e516fdcd
Noomaann <abdulkadernoman71@gmail.com>
2026-06-29 23:45:42 +0600

    Update schemas.py

--- AI Customer Support Ticket System/backend/schemas.py
+++ AI Customer Support Ticket System/backend/schemas.py
@@ -1,11 +1,10 @@
 from pydantic import BaseModel
 from typing import Optional
 
-# কাস্টমার যখন মেসেজ পাঠাবে, তখন শুধু এই ডেটাটুকু আসবে
+
 class TicketCreate(BaseModel):
     customer_message: str
 
-# API থেকে রেসপন্স হিসেবে আমরা যা ফেরত পাঠাব
 class TicketResponse(BaseModel):
     id: int
     customer_message: str
